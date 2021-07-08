bb99c6da798faa6e3054fafdca367d9e1d59d893
Joshua Jones <joshuajones2010@gmail.com>
2021-07-08 14:20:08 +1000

    Initial commit

---
+++ Editor/CustomEditors.cs
@@ -0,0 +1,160 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Dreemar.PackageTool
+{
+    [CustomEditor(typeof(AssemblyDefinition))]
+    public class NoScriptEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            DrawPropertiesExcluding(serializedObject, "m_Script");
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+
+    [CustomEditor(typeof(PackageInfo))]
+    public class PackageInfoEditor : Editor
+    {
+        SerializedProperty _name;
+        SerializedProperty _version;
+
+        SerializedProperty _author;
+        SerializedProperty _authorName;
+        SerializedProperty _authorEmail;
+        SerializedProperty _authorUrl;
+
+        private void Awake()
+        {
+            _name = serializedObject.FindProperty("name");
+            _version = serializedObject.FindProperty("version");
+
+            _author = serializedObject.FindProperty("author");
+            _authorName = _author.FindPropertyRelative("name");
+            _authorEmail = _author.FindPropertyRelative("email");
+            _authorUrl = _author.FindPropertyRelative("url");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            // Cache editor label colour
+            var labelColourDefault = EditorStyles.label.normal.textColor;
+            // The first few elements we draw will be required, so draw them in red
+            EditorStyles.label.normal.textColor = Color.red;
+
+            // Draw the package name field
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                _name.stringValue = EditorGUILayout.TextField("Name", _name.stringValue);
+                // Replace spaces with hyphens
+                _name.stringValue = _name.stringValue.Replace(" ", "-");
+            }
+
+            // Draw the version code field
+            // Looks like: [   ].[   ].[   ]
+            using(new EditorGUILayout.HorizontalScope())
+            {
+                EditorGUILayout.PrefixLabel("Version ");
+                GUILayout.Space(2);
+
+                string[] versionSplit = _version.stringValue.Split('.');
+
+                // Ensure there are only 3 elements in the string
+                string[] nVer = new string[3];
+                for (int i = 0; i < nVer.Length; i++)
+                {
+                    if (i < versionSplit.Length)
+                    {
+                        if (i < 2)
+                        {
+                            nVer[i] = versionSplit[i];
+                            // Ensure each item has a value
+                            if(string.IsNullOrEmpty(nVer[i]))
+                            {
+                                if (i == 0)
+                                    nVer[i] = "1";
+                                else
+                                    nVer[i] = "0";
+                            }
+
+                        }
+                        else
+                        {
+                            // Combine the remaining split strings
+                            // This may occur if the verion code looks something like
+                            // 1.0.0-preview-3.1
+                            nVer[i] = string.Join(".", versionSplit, i, versionSplit.Length - i);
+                        }
+                    }
+                    // Fill any remaining with zeroes
+                    // There will always be 1 element in versionSplit so this will always be x.[missing].[missing]
+                    // And the above code will ensure the first element has a value
+                    else
+                        nVer[i] = "0";
+                }
+                versionSplit = nVer;
+
+                // Draw gui for each item
+                for (int i = 0; i < versionSplit.Length; i++)
+                {
+                    versionSplit[i] = EditorGUILayout.TextField(versionSplit[i]);
+                    if(i < versionSplit.Length - 1)
+                        GUILayout.Label(".");
+                }
+
+                // Combine the items with a decimal between each
+                _version.stringValue = string.Join(".", versionSplit);
+                // Replace spaces with hyphens
+                _version.stringValue = _version.stringValue.Replace(" ", "-");
+            }
+
+            // Cache this in case the user has a different style set in some custom stuff
+            // Who even knows, you do you
+            var foldoutColourDefault = EditorStyles.foldout.normal.textColor;
+
+            /* 
+             * For whatever reason I can't for the life of me find how to change the colour of the
+             * foldout label when it's expanded. Not that big of a deal since we can just highlight the
+             * inner elements instead. Whatever.
+            */
+
+            // Draw the foldout with a red label (the name field is required)
+            EditorStyles.foldout.normal.textColor = Color.red;
+
+            // Draw the foldout for the author details
+            _author.isExpanded = EditorGUILayout.Foldout(_author.isExpanded, "Author");
+
+            // Reset to default colour
+            EditorStyles.foldout.normal.textColor = foldoutColourDefault;
+
+            if (_author.isExpanded)
+            {
+                EditorGUI.indentLevel++;
+
+                EditorGUILayout.PropertyField(_authorName);
+                // Other elements are not required, revert to default label colour
+                EditorStyles.label.normal.textColor = labelColourDefault;
+                EditorGUILayout.PropertyField(_authorEmail);
+                EditorGUILayout.PropertyField(_authorUrl);
+
+                EditorGUI.indentLevel--;
+            }
+
+            // Other elements are not required, revert to default label colour
+            EditorStyles.label.normal.textColor = labelColourDefault;
+
+            // Draw other fields with default editor fields
+            DrawPropertiesExcluding(serializedObject, "m_Script", "name", "version", "author");
+
+            // TODO: Create a property drawer for the Unity Version field
+            // (to emulate the one shown in the package.json inspector)
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Editor/PackageCreatorWindow.cs
@@ -0,0 +1,200 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using System.IO;
+using System.Linq;
+
+namespace Dreemar.PackageTool
+{
+    #region Package Information Classes
+    public class AssemblyDefinition : ScriptableObject
+    {
+        new public string name;
+        public string rootNamespace;
+        [HideInInspector]
+        public string[] includePlatforms;
+        [HideInInspector]
+        public string[] references;
+    }
+
+    public class PackageInfo : ScriptableObject
+    {
+        [System.Serializable]
+        public class PackageAuthor
+        {
+            public string name;
+            public string email;
+            public string url;
+        }
+
+        new public string name;
+        public string version = "1.0.0";
+        public string displayName;
+        public string unity;
+        public string unityRelease;
+        public PackageAuthor author;
+        [Multiline]
+        public string description;
+    }
+    #endregion
+
+    public class PackageCreatorWindow : EditorWindow
+    {
+        #region Static Methods
+        [MenuItem("Tools/Create Package")]
+        public static void OpenWindow()
+        {
+            var window = GetWindow<PackageCreatorWindow>();
+            window.titleContent = new GUIContent("Package Creator");
+            window.Show();
+        }
+        #endregion
+
+        #region Private Fields
+        PackageInfo _packageInfo;
+        AssemblyDefinition _runtimeAssembly;
+        AssemblyDefinition _editorAssembly;
+
+        List<(Editor editor, string label)> _editors;
+
+        GUIStyle _headerStyle;
+        #endregion
+
+        #region Initialization
+        void Startup()
+        {
+            // Make a nice lil header label style
+            _headerStyle = new GUIStyle(GUI.skin.label);
+            _headerStyle.fontSize = 25;
+            _headerStyle.fontStyle = FontStyle.Bold;
+
+            // Setup the serializable items
+            if (_packageInfo == null)
+                _packageInfo = CreateInstance<PackageInfo>();
+
+            if (_runtimeAssembly == null)
+                _runtimeAssembly = CreateInstance<AssemblyDefinition>();
+            if (_editorAssembly == null)
+            {
+                _editorAssembly = CreateInstance<AssemblyDefinition>();
+                // Ensure the editor assembly definition only includes the "Editor" platform
+                // If includePlatforms is left empty, all platforms will be included
+                _editorAssembly.includePlatforms = new[] { "Editor" };
+            }
+
+            CreateEditors();
+        }
+
+        void CreateEditors()
+        {
+            _editors = new List<(Editor editor, string label)>();
+
+            // Package info
+            _editors.Add((Editor.CreateEditor(_packageInfo), "Package Info"));
+            // Runtime assembly
+            _editors.Add((Editor.CreateEditor(_runtimeAssembly), "Runtime Assembly Definition"));
+            // Editor assembly
+            _editors.Add((Editor.CreateEditor(_editorAssembly), "Editor Assembly Definition"));
+        }
+        #endregion
+
+        #region Drawing
+        private void OnGUI()
+        {
+            if (_editors == null || _editors.Count == 0 || _editors.Any(x => x.editor == null))
+            {
+                Startup();
+            }
+
+            using (new EditorGUILayout.VerticalScope(GUILayout.Width(position.width)))
+            {
+                foreach (var item in _editors)
+                {
+                    EditorGUILayout.Space();
+                    // Draw a nice little label for the editor c:
+                    GUILayout.Label(item.label, EditorStyles.boldLabel);
+                    item.editor.OnInspectorGUI();
+                }
+
+                EditorGUILayout.Space();
+
+                GUILayout.Label("Note: The Editor Assembly Definition will reference the Runtime Assembly via name. " +
+                    "It is recommended that you select the \"Use GUIDs\" toggle if you intend to reference " +
+                    "other assemblies outside the package. (This will automatically convert any existing references to use GUIDs)",
+                    EditorStyles.wordWrappedLabel);
+
+                if (GUILayout.Button("Create"))
+                {
+                    CreatePackage();
+                }
+            }
+        }
+        #endregion
+
+        #region Package Generation
+        void CreatePackage()
+        {
+            if (string.IsNullOrEmpty(_packageInfo.name) || string.IsNullOrEmpty(_packageInfo.version) ||
+                string.IsNullOrEmpty(_packageInfo.author.name))
+            {
+                Debug.LogError("Required fields (marked in red) cannot be empty.");
+                return;
+            }
+
+            var rootPath = Path.Combine(Application.dataPath, "../", "Packages", _packageInfo.name);
+            if (Directory.Exists(rootPath))
+            {
+                Debug.LogError("Package already exists in Packages directory");
+                return;
+            }
+
+            // Format the package name
+            _packageInfo.name = $"com.{_packageInfo.author.name.Replace(" ", "-")}.{_packageInfo.name}".ToLower();
+
+            // Create directories
+            Directory.CreateDirectory(rootPath);
+            Directory.CreateDirectory(Path.Combine(rootPath, "Documentation~"));
+            Directory.CreateDirectory(Path.Combine(rootPath, "Samples~"));
+            Directory.CreateDirectory(Path.Combine(rootPath, "Editor"));
+            Directory.CreateDirectory(Path.Combine(rootPath, "Runtime"));
+
+            // Create empty markdown files
+            File.Create(Path.Combine(rootPath, "Documentation~", $"{_packageInfo.name}.md"));
+            File.Create(Path.Combine(rootPath, "CHANGELOG.md"));
+            File.Create(Path.Combine(rootPath, "README.md"));
+            
+            // Create the package.json file from the package information
+            File.WriteAllText(Path.Combine(rootPath, "package.json"), JsonUtility.ToJson(_packageInfo));
+
+            if (!string.IsNullOrEmpty(_runtimeAssembly.name))
+            {
+                // Create runtime assembly
+                File.WriteAllText(Path.Combine(rootPath, "Runtime", $"{_runtimeAssembly.name}.asmdef"), JsonUtility.ToJson(_runtimeAssembly));
+
+                // Reference the runtime assembly in the editor. This will store as name and the user can opt to change it to use GUIDs once the package is generated.
+                _editorAssembly.references = new[] { _runtimeAssembly.name };
+            }
+
+            if (!string.IsNullOrEmpty(_editorAssembly.name))
+            {
+                // Create runtime assembly
+                File.WriteAllText(Path.Combine(rootPath, "Editor", $"{_editorAssembly.name}.asmdef"), JsonUtility.ToJson(_editorAssembly));
+            }
+
+            Debug.Log($"Successfully created new package: {_packageInfo.name}");
+
+            _packageInfo = null;
+            _runtimeAssembly = null;
+            _editorAssembly = null;
+            _editors = null;
+
+            // Tell the package manager to resolve packages
+            Client.Resolve();
+            // Package creation complete, close the window
+            Close();
+        }
+        #endregion
+    }
+}
